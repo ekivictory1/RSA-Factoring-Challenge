058c8d4e30fba3d7a38c9689e1149ddcc96ca990
ekivictory1 <106748977+ekivictory1@users.noreply.github.com>
2022-10-10 10:00:39 +0100

    RSA

--- rsa-rs/src/main.rs
+++ rsa-rs/src/main.rs
@@ -1,19 +1,20 @@
-// (C) 2019 Fernando Gonzalez-Morales
+// (C) 2022 Eki Victory
 // That's right.
 #![allow(irrefutable_let_patterns)]
 use std::{env, fs};
-use ramp::Int;
+use ramp::int;
+use rayon::prelude::*;
 
 
 fn main() {
-    let primes: Vec<Int> = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+    let primes: Vec<int> = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
                             31, 37, 41, 43, 47, 53, 59, 61, 67,
                             71, 73, 79, 83, 89, 97].into_iter()
-                                                   .map(|x| Int::from(*x))
+                                                   .map(|x| int::from(*x))
                                                    .collect();
     let filename = env::args().nth(1).unwrap();
     let contents = fs::read_to_string(filename).unwrap();
-    let n: Int = contents.lines().nth(0).unwrap().parse().unwrap();
+    let n: int = contents.lines().nth(0).unwrap().parse().unwrap();
     if n < 10000 {
         let (p, q) = look_up(&n, &primes);
         println!("{}={}*{}", n, p, q);
@@ -25,25 +26,25 @@ fn main() {
 }
 
 /* https://stackoverflow.com/a/2274520/9221785 */
-fn look_up(n: &Int, ps: &Vec<Int>) -> (Int, Int) {
+fn look_up(n: &int, ps: &Vec<int>) -> (int, int) {
     for p in ps.iter() {
 		if let (q, r) = n.divmod(p) {
             if r == 0 { return (p.clone(), q) };
         }
     }
-    (Int::one(), n.clone())
+    (int::one(), n.clone())
 }
 
 /* Refs:
  * +http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.117.1230&rep=rep1&type=pdf
  * +https://comeoncodeon.wordpress.com/2010/09/18/pollard-rho-brent-integer-factorization/
  */
-fn pollard_brent(num: &Int) -> (Int, Int) {
-    let (mut x, mut y) = (Int::from(2), Int::from(2));
-    let (mut d, mut q) = (Int::one(), Int::one());
-    let (mut ys, mut r) = (Int::zero(), 1);
+fn pollard_brent(num: &int) -> (int, int) {
+    let (mut x, mut y) = (int::from(2), int::from(2));
+    let (mut d, mut q) = (int::one(), int::one());
+    let (mut ys, mut r) = (int::zero(), 1);
     const M: i32 = 71;
-    let f = |i: &Int| (i.pow_mod(&Int::from(2), &num) + M) % num;
+    let f = |i: &int| (i.pow_mod(&int::from(2), &num) + M) % num;
     while d == 1 {
         x = f(&y);
         for _ in 0..r {
